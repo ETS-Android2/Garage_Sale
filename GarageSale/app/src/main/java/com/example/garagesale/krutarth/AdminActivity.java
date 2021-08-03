9e61b05775139e60f6be7c3ed68a20cead884632
Krutarth1512 <krutarth.rangunwala@avm.edu.in>
2021-08-02 21:29:06 -0400

    Admin activity added

--- GarageSale/app/src/main/java/com/example/garagesale/krutarth/AdminActivity.java
+++ GarageSale/app/src/main/java/com/example/garagesale/krutarth/AdminActivity.java
@@ -5,6 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.garagesale.R;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.garagesale.mehul.HomeActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -12,5 +27,60 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_products:
+                        startActivity(new Intent(AdminActivity.this, AdminProductsActivity.class));
+                        break;
+
+                    case R.id.navigation_customers:
+                        break;
+
+                    case R.id.navigation_transactions:
+                        break;
+
+                    case R.id.navigation_logout:
+                        logoutAdmin();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        getProductCount();
+        getCustomerCount();
+    }
+
+    private void getProductCount() {
+    }
+
+    private void getCustomerCount() {
+
+    }
+
+    private void logoutAdmin() {
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Logout")
+                .setMessage("Are you are sure want to logout ?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(AdminActivity.this, HomeActivity.class));
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
     }
 }
