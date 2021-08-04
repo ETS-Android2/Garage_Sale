b9649609dc97ff89654ca5465930bac807055dbd
Harsh Patel <hdpatel.djp257@gmail.com>
2021-08-04 01:39:33 -0400

    All products Activity added

--- GarageSale/app/src/main/java/com/example/garagesale/harsh/AllProductActivity.java
+++ GarageSale/app/src/main/java/com/example/garagesale/harsh/AllProductActivity.java
@@ -1,16 +1,53 @@
 package com.example.garagesale.harsh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import com.example.garagesale.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllProductActivity extends AppCompatActivity {
+
+    private RecyclerView mProductRecycler;
+    private AllProductAdapter mAllProductAdapter;
+    private final List<Product> mProductList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_product);
+
+        mProductRecycler = findViewById(R.id.rcv_product);
+
+        FirebaseFirestore.getInstance().collection("Products").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                mProductList.add(document.toObject(Product.class));
+                            }
+                            mAllProductAdapter = new AllProductAdapter(mProductList);
+                            mProductRecycler.setAdapter(mAllProductAdapter);
+
+                        } else {
+                            Toast.makeText(AllProductActivity.this, "Error getting products", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
